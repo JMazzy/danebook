3469cd431e45e6200d94145e9a19b5f88355ab99
Joshua Masland-Sarani <j.masland@gmail.com>
2016-02-18 20:12:08 -0500

    revamped routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,22 @@
 Rails.application.routes.draw do
-
   root "users#new"
 
-  resources :users do
-    resources :posts
-  end
+  resources :users
+  get '/timeline/:id', to: 'users#show', as: 'timeline'
+  get '/friends/:id', to: 'users#index', as: 'friends'
 
-  resources :posts do
-    resources :comments
-    resources :likes
-  end
-
-  resources :comments do
-    resources :likes
-  end
-
-  resources :likes
-
-  resources :friendings, only: [:create,:destroy]
+  resources :profiles, only: [:show, :edit, :update]
 
   resource :session, :only => [:new, :create, :destroy]
   get "login" => "sessions#new"
   delete "logout" => "sessions#destroy"
 
-  resources :profiles
+  resources :posts, only: [:index, :create, :destroy]
+  get '/about/:id', to: 'profiles#show', as: 'about'
 
+  resources :comments, only: [:create, :destroy]
+
+  resources :likes, only: [:create, :destroy]
+
+  resources :friendings, only: [:create,:destroy]
 end
